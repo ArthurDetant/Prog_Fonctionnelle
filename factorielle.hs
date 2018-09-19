5b63156b9657b586fcb92d04db9bfa7dff0e69f2
ArthurDetant <arthur.detant1996@gmail.com>
2018-09-19 13:53:31 +0200

    ds

--- factorielle.hs
+++ factorielle.hs
@@ -1,1 +1,18 @@
-f
+import System.Environment
+
+factorielle :: Int -> Int
+factorielle n = if n==1
+                then 1
+                else n * factorielle (n-1)
+
+factorielle' = aux 1
+              where aux acc 0 = acc
+                    aux acc n = aux (acc*n) (n-1)               
+                
+
+main :: IO()
+main = do
+
+print $ factorielle 4
+print $ factorielle 4
+
